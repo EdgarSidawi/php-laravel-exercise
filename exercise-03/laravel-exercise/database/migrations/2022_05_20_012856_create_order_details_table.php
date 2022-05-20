ee12d8e3642d10110e36ea3d78d4a8ed23232c4f
Edgar <romantibuai@gmail.com>
2022-05-20 10:04:48 +0000

    Update exercise-02 and orderDetails migration

--- exercise-03/laravel-exercise/database/migrations/2022_05_20_012856_create_order_details_table.php
+++ exercise-03/laravel-exercise/database/migrations/2022_05_20_012856_create_order_details_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('order_details', function (Blueprint $table) {
             $table->id();
 
-            $table->integer('order_id');
+            $table->foreignId('order_id')->constrained();
             $table->integer('product_id');
             $table->decimal('unitPrice');
             $table->integer('quantity');
